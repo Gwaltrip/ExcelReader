d2d2a0fb477b14efea9fc7b838fff7676fbc4e22
Gwaltrip <gabrielwaltrip@gmail.com>
2015-03-22 03:37:14 -0700

    Main

---
+++ ExcelReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelReader
+{
+    class ExcelReader
+    {
+        private Excel.Application xlApp;
+        private Excel.Workbook xlWBook;
+        private Excel.Worksheet xlSheet;
+        private Excel.Range range;
+
+
+        public ExcelReader(string path)
+        {
+            xlApp = new Excel.Application();
+            xlWBook = xlApp.Workbooks.Open(path);
+            xlSheet = (Excel.Worksheet)xlWBook.Worksheets.get_Item(1);
+            range = xlSheet.UsedRange;
+        }
+
+        public List<double> Range(int col)
+        {
+            List<double> list = new List<double>();
+            int lastRow = xlSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+
+            for (int i = 1; i <= lastRow; i++)
+            {
+                list.Add(Double.Parse((range.Cells[i, col] as Excel.Range).Value2.ToString()));
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace ExcelReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase),"ExcelBook.xlsx");
+            ExcelReader exr = new ExcelReader(new Uri(file).LocalPath);
+            List<double> list = exr.Range(1);
+
+            Console.WriteLine("Average: " + list.Average());
+            Console.WriteLine("Sum: " + list.Sum());
+            Console.ReadKey();
+        }
+    }
+}
